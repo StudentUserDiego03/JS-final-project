5c054adf17259c2e2daeb19f16f8012bffc9474f
StudentUserDiego03 <dcardenascastane@student.alamo.edu>
2024-04-09 21:31:23 -0500

    created hide/show-based functions

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
+
+const registerScreen = document.querySelector(".register");
+const Thanks = document.querySelector(".thanks");
+const accountSection = document.querySelector(".account");
+
 function register(){
     let userName = document.getElementById("user-name").value;
     let Password = document.getElementById("password").value;
     localStorage.setItem("USERNAME", userName );
     localStorage.setItem("PASSWORD", Password );
+    if(!registerScreen.classList.contains("not-Active")){
+    registerScreen.classList.add("not-Active")}
+    if(Thanks.classList.contains("not-Active")){
+        Thanks.classList.remove("not-Active")
+    }
 }
 
 function log(){
     let userNameLog = document.getElementById("info-1").value;
     let PasswordLog = document.getElementById("info-2").value;
-    let code1 = localStorage.getItem("USERNAME");
-    let code2 = localStorage.getItem("PASSWORD");
-    if(!userNameLog==code1){
-        document.getElementById("error").innerHTML="wrong info"
+    if(accountSection.classList.contains("not-Active")){
+        accountSection.classList.remove("not-Active")
     }
+    if(!registerScreen.classList.contains("not-Active")){
+        registerScreen.classList.add("not-Active")
+    }
+    document.getElementById("Welcome").innerHTML="Welcome back " + localStorage.getItem("USERNAME");
+    
 }
